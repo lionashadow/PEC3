9ff85ee8738ea4ac1d3e4ec1b681b9683b432b23
lionashadow <78237188+lionashadow@users.noreply.github.com>
2021-12-23 23:26:44 +0100

    Update Scripts.R

--- Scripts.R
+++ Scripts.R
@@ -21,29 +21,51 @@ colnames(column_2C) <- colnames(column_3C)
 
 colnames(column_2C) <- colnames(column_3C)
 
-column_2C<-rename(column_2C, "2C_Class" = "class")
-column_3C<-rename(column_3C, "3C_Class" = "class")
+column_2C<-rename(column_2C, "class_2" = "class")
+column_3C<-rename(column_3C, "class_3" = "class")
+
+
 ortho_df <- merge(column_2C, column_3C, by = c("pelvic_incidence", "pelvic_tilt", "lumbar_lordosis_angle", "sacral_slope", "pelvic_radius", "degree_spondylolisthesis"))
-ortho_df <- ortho_df %>% mutate("2C_Class" = factor(ortho_df$`2C_Class`, levels = c("Normal", "Abnormal")))
-ortho_df <- ortho_df %>% mutate("3C_Class" = factor(ortho_df$`3C_Class`, levels = c("Normal", "Hernia", "Spondylolisthesis")))
+ortho_df <- ortho_df %>% mutate("class_2" = factor(ortho_df$`class_2`, levels = c("Normal", "Abnormal")))
+ortho_df <- ortho_df %>% mutate("class_3" = factor(ortho_df$`class_3`, levels = c("Normal", "Hernia", "Spondylolisthesis")))
+ortho_df2 <- ortho_df %>% pivot_longer(cols = c(pelvic_incidence, pelvic_tilt, pelvic_radius, sacral_slope, lumbar_lordosis_angle), names_to = "type_of_measure", values_to = "angles") %>% select(type_of_measure, angles, everything())
+ortho_df3 <- ortho_df %>% select(where(is.numeric)) 
 
 summary(ortho_df)
 summary(sapply(ortho_df,is.na))
 
-ortho_df %>% select(-c(`2C_Class`, `3C_Class`)) %>% pairs()
-model.matrix(~0+., ortho_df) %>%cor() %>% ggcorrplot(lab=T, lab_size=2.5,type = "lower")
+ortho_df %>% select(where(is.numeric)) %>% pairs()
+model.matrix(~0. + pelvic_incidence+pelvic_tilt+pelvic_radius+sacral_slope+lumbar_lordosis_angle+ degree_spondylolisthesis + class_3, ortho_df) %>%cor() %>% ggcorrplot(lab=T, lab_size=2.5,type = "lower")
+model.matrix(~0. + pelvic_incidence+pelvic_tilt+pelvic_radius+sacral_slope+lumbar_lordosis_angle+ degree_spondylolisthesis + class_3, ortho_df) %>%cor() %>% ggcorrplot(lab=T, lab_size=2.5,type = "lower")
+
 # Visualization part
 
-ortho_df2 <- ortho_df %>% pivot_longer(cols = c(pelvic_incidence, pelvic_tilt, pelvic_radius, sacral_slope, lumbar_lordosis_angle), names_to = "type_of_measure", values_to = "angles")
-ortho_df2 <- ortho_df2 %>% select(type_of_measure, angles, everything())
-ortho_df2 %>% ggplot(aes(angles, fill = type_of_measure)) + geom_density(alpha = 0.2)+ facet_grid(~`2C_Class`)
-ortho_df2 %>% ggplot(aes(angles, fill = type_of_measure)) + geom_density(alpha = 0.2)+ facet_grid(~`3C_Class`)
-ortho_df2 %>% ggplot(aes(y= angles, color = type_of_measure)) + geom_boxplot() + facet_grid(~`2C_Class`)
-ortho_df2 %>% ggplot(aes(y= angles, color = type_of_measure)) + geom_boxplot() + facet_grid(~`3C_Class`)
-ortho_df2 %>% ggplot(aes(x= angles,y = degree_spondylolisthesis,  color = type_of_measure)) + geom_point() + facet_grid(~`2C_Class`)
-ortho_df2 %>% ggplot(aes(x= angles,y = degree_spondylolisthesis,  color = type_of_measure)) + geom_point() + facet_grid(~`3C_Class`)
+
+ortho_df2 %>% ggplot(aes(angles, fill = type_of_measure)) + geom_density(alpha = 0.2)+ facet_grid(~class_2)
+ortho_df2 %>% ggplot(aes(angles, fill = type_of_measure)) + geom_density(alpha = 0.2)+ facet_grid(~class_3)
+ortho_df2 %>% ggplot(aes(y= angles, color = type_of_measure)) + geom_boxplot() + facet_grid(~class_2)
+ortho_df2 %>% ggplot(aes(y= angles, color = type_of_measure)) + geom_boxplot() + facet_grid(~class_3)
+ortho_df2 %>% ggplot(aes(x= angles,y = degree_spondylolisthesis,  color = type_of_measure)) + geom_point() + facet_grid(~class_2)
+ortho_df2 %>% ggplot(aes(x= angles,y = degree_spondylolisthesis,  color = type_of_measure)) + geom_point() + facet_grid(~class_3)
+ortho_df2 %>% ggplot(aes(x =class_3, y= degree_spondylolisthesis, color = type_of_measure)) + geom_boxplot()
 
 # Normality test
+
+## Kolmogorov-Smirnov
+
 ortho_df %>% select(where(is.numeric))%>% colnames() %>%
-  set_names() %>%  map(~ ks.test(ortho_df[,.x], "pbinom")) %>%
+  set_names() %>%  map(~ ks.test(ortho_df[,.x], "pnorm")) %>%
   map_dfr(., tidy, .id = "parameter")
+ks.test(ortho_df2$degree_spondylolisthesis[ortho_df2$class_3== "Hernia"], "pnorm")
+## Shapiro
+
+ortho_df %>% select(where(is.numeric))%>% colnames() %>%
+  set_names() %>%  map(~ shapiro.test(ortho_df[,.x])) %>%
+  map_dfr(., tidy, .id = "parameter")
+shapiro.test(ortho_df2$degree_spondylolisthesis[ortho_df2$class_3== "Hernia"])
+## QQplot
+gqqplot(ortho_df2, "angles", facet.by = "class_3", color = "type_of_measure")
+ggqqplot(ortho_df2, "angles", facet.by = "class_2", color = "type_of_measure")
+ggqqplot(ortho_df2, "degree_spondylolisthesis", color = "class_3", facet.by  = "class_2")
+
+
